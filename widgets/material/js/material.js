de9540335694eb6b5a1a3e7da3a6cebdd205cb79
EdgarM73 <edgar.miller@gmail.com>
2020-07-12 13:21:49 +0200

    new image

--- widgets/material/js/material.js
+++ widgets/material/js/material.js
@@ -9,19 +9,51 @@
 // add translations for edit mode
 if (vis.editMode) {
     $.extend(true, systemDictionary, {
-        "title":          {"en": "Title",       "de": "Titel",  "ru": "Заголовок"},
-        "subtitle":       {"en": "Subtitle",      "de": "Untertitel",   "ru": "Подзаголовок"}
+        "title": {
+            "en": "Title",
+            "de": "Titel",
+            "ru": "Заголовок"
+        },
+        "subtitle": {
+            "en": "Subtitle",
+            "de": "Untertitel",
+            "ru": "Подзаголовок"
+        }
     });
 }
 
 // add translations for non-edit mode
 $.extend(true, systemDictionary, {
-    "Instance":     {"en": "Instance", "de": "Instanz", "ru": "Инстанция"},
-    "open":         {"en": "open", "de": "offen", "ru": "открыто"},
-    "tilted":       {"en": "tilted", "de": "gekippt", "ru": "приоткрыто"},
-    "closed":       {"en": "closed", "de": "geschlossen", "ru": "закрыто"},
-    "on":           {"en": "on", "de": "an", "ru": "вкл"},
-    "off":          {"en": "off", "de": "aus", "ru": "выкл"}
+    "Instance": {
+        "en": "Instance",
+        "de": "Instanz",
+        "ru": "Инстанция"
+    },
+    "open": {
+        "en": "open",
+        "de": "offen",
+        "ru": "открыто"
+    },
+    "tilted": {
+        "en": "tilted",
+        "de": "gekippt",
+        "ru": "приоткрыто"
+    },
+    "closed": {
+        "en": "closed",
+        "de": "geschlossen",
+        "ru": "закрыто"
+    },
+    "on": {
+        "en": "on",
+        "de": "an",
+        "ru": "вкл"
+    },
+    "off": {
+        "en": "off",
+        "de": "aus",
+        "ru": "выкл"
+    }
 });
 
 vis.binds.material = {
@@ -32,7 +64,7 @@ vis.binds.material = {
             vis.binds.material.version = null;
         }
     },
-	tplMdListDoor: function (widgetID, view, data) {
+    tplMdListDoor: function (widgetID, view, data) {
         const srcOpen = 'widgets/material/img/fts_door_open.png';
         const srcClosed = 'widgets/material/img/fts_door.png';
         const valOpen = _('open');
@@ -46,7 +78,7 @@ vis.binds.material = {
             }, 100);
         }
 
-        function update(state){
+        function update(state) {
             var value = (state) ? valOpen : valClosed;
             var src = (state) ? srcOpen : srcClosed;
             $div.find('.md-list-value').html(value);
@@ -63,7 +95,7 @@ vis.binds.material = {
             update(vis.states[data.oid + '.val']);
         }
     },
-	tplMdListWindow: function (widgetID, view, data) {
+    tplMdListWindow: function (widgetID, view, data) {
         const srcOpen = 'widgets/material/img/fts_window_2w_open.png';
         const srcClosed = 'widgets/material/img/fts_window_2w.png';
         const valOpen = _('open');
@@ -77,13 +109,13 @@ vis.binds.material = {
             }, 100);
         }
 
-        function update(state){
+        function update(state) {
             var value = (state) ? valOpen : valClosed;
             var src = (state) ? srcOpen : srcClosed;
             $div.find('.md-list-value').html(value);
             $div.find('.md-list-icon').find('img').attr('src', src);
-        }    
-        
+        }
+
         if (data.oid) {
             // subscribe on updates of value
             vis.states.bind(data.oid + '.val', function (e, newVal, oldVal) {
@@ -103,17 +135,17 @@ vis.binds.material = {
                 vis.binds.material.tplMdListTemp(widgetID, view, data);
             }, 100);
         }
-        
+
         // grey out the value in case the last change is more than 24h ago
         var curTime = new Date().getTime();
         var lcTime = vis.states[data.oid + '.lc'];
         var seconds = (curTime - lcTime) / 1000;
-        if(seconds > 86400){ 
+        if (seconds > 86400) {
             $div.find('.md-list-value').css('opacity', '0.5');
         }
-        
-        function update(state){
-            if(typeof state === 'number'){
+
+        function update(state) {
+            if (typeof state === 'number') {
                 $div.find('.md-list-value').html(state.toFixed(1) + ' °C');
             }
         }
@@ -137,17 +169,17 @@ vis.binds.material = {
                 vis.binds.material.tplMdListHumid(widgetID, view, data);
             }, 100);
         }
-        
+
         // grey out the value in case the last change is more than 24h ago
         var curTime = new Date().getTime();
         var lcTime = vis.states[data.oid + '.lc'];
         var seconds = (curTime - lcTime) / 1000;
-        if(seconds > 86400){ 
+        if (seconds > 86400) {
             $div.find('.md-list-value').css('opacity', '0.5');
         }
-        
-        function update(state){
-            if(typeof state === 'number'){
+
+        function update(state) {
+            if (typeof state === 'number') {
                 $div.find('.md-list-value').html(state.toFixed(1) + ' %');
             }
         }
@@ -171,23 +203,21 @@ vis.binds.material = {
                 vis.binds.material.tplMdListOccupancy(widgetID, view, data);
             }, 100);
         }
-        
+
         // grey out the value in case the last change is more than 24h ago
         var curTime = new Date().getTime();
         var lcTime = vis.states[data.oid + '.lc'];
         var seconds = (curTime - lcTime) / 1000;
-        if(seconds > 86400){ 
+        if (seconds > 86400) {
             $div.find('.md-list-value').css('opacity', '0.5');
         }
-        
-        function update(state){
-            if( state){
+
+        function update(state) {
+            if (state) {
                 $div.find('.md-list-value').html('Bewegung');
+            } else {
+                $div.find('.md-list-value').html('Nein');
             }
-            else 
-           {
-            $div.find('.md-list-value').html('Keine Bewegung');
-           } 
         }
 
         if (data.oid) {
@@ -200,7 +230,7 @@ vis.binds.material = {
             update(vis.states[data.oid + '.val']);
         }
     },
-	tplMdListLight: function (widgetID, view, data) {
+    tplMdListLight: function (widgetID, view, data) {
         const srcOff = 'widgets/material/img/light_light_dim_00.png';
         const srcOn = 'widgets/material/img/light_light_dim_100.png';
         var $div = $('#' + widgetID);
@@ -212,7 +242,7 @@ vis.binds.material = {
             }, 100);
         }
 
-        function update(state){
+        function update(state) {
             var src = (state) ? srcOn : srcOff;
             var $tmp = $('#' + widgetID + '_checkbox');
             $tmp.prop('checked', state);
@@ -226,7 +256,7 @@ vis.binds.material = {
                 vis.setValue($this_.data('oid'), $this_.prop('checked'));
             });
         }
-        
+
         if (data.oid) {
             // subscribe on updates of value
             vis.states.bind(data.oid + '.val', function (e, newVal, oldVal) {
@@ -237,7 +267,7 @@ vis.binds.material = {
             update(vis.states[data.oid + '.val']);
         }
     },
-	tplMdListLightDim: function (widgetID, view, data) {
+    tplMdListLightDim: function (widgetID, view, data) {
         const srcOff = 'widgets/material/img/light_light_dim_00.png';
         const srcOn = 'widgets/material/img/light_light_dim_100.png';
         var $div = $('#' + widgetID);
@@ -249,8 +279,8 @@ vis.binds.material = {
             }, 100);
         }
 
-        function update(state){
-            var src = 'widgets/material/img/light_light_dim_' + Math.ceil(state/10) + '0.png';
+        function update(state) {
+            var src = 'widgets/material/img/light_light_dim_' + Math.ceil(state / 10) + '0.png';
             $div.find('.md-list-icon').find('img').attr('src', src);
         }
 
@@ -261,7 +291,7 @@ vis.binds.material = {
                 vis.setValue($this_.data('oid'), $this_.prop('checked'));
             });
         } */
-        
+
         if (data.oid) {
             // subscribe on updates of value
             vis.states.bind(data.oid + '.val', function (e, newVal, oldVal) {
@@ -272,7 +302,7 @@ vis.binds.material = {
             update(vis.states[data.oid + '.val']);
         }
     },
-	tplMdListShutter: function (widgetID, view, data) {
+    tplMdListShutter: function (widgetID, view, data) {
         const srcOff = 'widgets/material/img/fts_shutter_00.png';
         const srcOn = 'widgets/material/img/fts_shutter_100.png';
         var $div = $('#' + widgetID);
@@ -284,8 +314,8 @@ vis.binds.material = {
             }, 100);
         }
 
-        function update(state){
-            var src = 'widgets/material/img/fts_shutter_' + Math.ceil(state/10) + '0.png';
+        function update(state) {
+            var src = 'widgets/material/img/fts_shutter_' + Math.ceil(state / 10) + '0.png';
             $div.find('.md-list-icon').find('img').attr('src', src);
         }
 
@@ -296,7 +326,7 @@ vis.binds.material = {
                 vis.setValue($this_.data('oid'), $this_.prop('checked'));
             });
         } */
-        
+
         if (data.oid) {
             // subscribe on updates of value
             vis.states.bind(data.oid + '.val', function (e, newVal, oldVal) {
