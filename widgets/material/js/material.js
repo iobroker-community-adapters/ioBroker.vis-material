2e1b32edbec7af5daa556f8a50ce67b8c2607f45
Erdal Akkaya <erdal@sm.fritz.box>
2020-08-10 21:27:36 +0200

    new icons and test

--- widgets/material/js/material.js
+++ widgets/material/js/material.js
@@ -365,6 +365,7 @@ vis.binds.material = {
         }
 
         function update(state) {
+            var drittel = (data.attr('max') - data.attr('min')) / 3;
             var src = 'widgets/material/img/light_light_dim_' + Math.ceil(state / 10) + '0.png';
             $div.find('.md-list-icon').find('img').attr('src', src);
         }
