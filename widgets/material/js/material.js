2a86f3b47b8921b07dda960fa5b3841f13e332aa
Erdal Akkaya <erdal@sm.fritz.box>
2020-08-10 08:14:34 +0200

    removed $this

--- widgets/material/js/material.js
+++ widgets/material/js/material.js
@@ -391,9 +391,9 @@ vis.binds.material = {
         const srcOff = 'widgets/material/img/light_light_dim_00.png';
         const srcOn = 'widgets/material/img/light_light_dim_100.png';
         var $div = $('#' + widgetID);
-        var min = $this.attr('min');
+        /* var min = $this.attr('min');
         var max = $this.attr('max');
-
+*/
         // if nothing found => wait
         if (!$div.length) {
             return setTimeout(function() {
