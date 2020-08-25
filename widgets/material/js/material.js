30ea9cc1e7ed4005e9c42b8ad102942816666f9b
Erdal Akkaya <erdal@sm.fritz.box>
2020-08-25 20:59:44 +0200

    bugfix

--- widgets/material/js/material.js
+++ widgets/material/js/material.js
@@ -247,7 +247,7 @@ vis.binds.material = {
                 $div.find('.md-list-value').html(state.toFixed(1) + ' °C');
             }
             if (state <= $low) {
-                console.log('Temperatur ist niedrig');
+                console.log('Temperatur ist niedrig ');
                 $div.find('.my_child').removeClass('opac-white opac-red opac-below opac-above').addClass('opac-below');
             }
             if (state >= $normal) {
